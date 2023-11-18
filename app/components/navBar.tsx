3cc0c2dd003d59fd0479f410dc2ee387ec0833ed
paulano11 <ap3742@drexel.edu>
2023-11-18 01:44:58 -0500

    made bg images non clickable

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -1,4 +1,5 @@
 'use client';
+import './navbar.css'
 export function Navbar() {
     return (
         <div className="navbar bg-transparent text-neutral-content" style={{ 
@@ -9,7 +10,7 @@ export function Navbar() {
             zIndex: 100 
         }}>
             <div className="flex justify-end space-x-8 w-full pr-30%">
-                <button className="btn glass btn-xs sm:btn-sm md:btn-md lg:btn-lg text-white font-bold">About us</button>
+            <button className="btn glass btn-xs sm:btn-sm md:btn-md lg:btn-lg text-white font-bold blue-button">About us</button>
                 <button className="btn glass btn-xs sm:btn-sm md:btn-md lg:btn-lg text-white font-bold">FAQ</button>
                 <button className="btn glass btn-xs sm:btn-sm md:btn-md lg:btn-lg text-white font-bold">Contact us</button>
             </div>
