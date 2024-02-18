35d04f4c1bfa746652e2e67f6a349c6c5a193eb7
paulano11 <ap3742@drexel.edu>
2024-02-18 12:38:24 -0500

    fixed mail link

--- app/components/homePage.tsx
+++ app/components/homePage.tsx
@@ -43,7 +43,7 @@ export default function HomePage() {
             <h1 className="responsive-heading font-bold my-5 gradient-text">Dragon Hacks turns 10!</h1>
             <p className="responsive-text uppercase tracking-widest mb-10">
               Interested in sponsoring?
-              <a href="mailto:sponsor@dragonhacks.com" className="text-blue-500 underline">Contact us!</a>
+              <a href="mailto:teamdragonhacks@gmail.com" className="text-blue-500 underline">Contact us!</a>
             </p>
             <Link href="/register">
             <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-lg px-6 py-3 text-center me-2 mb-2">
